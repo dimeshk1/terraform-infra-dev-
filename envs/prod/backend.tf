4c034d53e6e1c2a73391324b1e6525122c480cc4
dimeshk1 <mistermovied@gmail.com>
2026-07-20 00:21:18 +0530

    added ec2 option

--- envs/prod/backend.tf
+++ envs/prod/backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
     bucket       = "terraform-bootstrap-dev-107282187171-state"
-    key          = "prod/terraform.tfstate"
+    #key          = "prod/terraform.tfstate"
+    key         = "prod/s3-prod-state-v1"
     region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
--- envs/prod/variables.tf
+++ envs/prod/variables.tf
@@ -20,3 +20,18 @@ variable "bucket_names" {
   description = "Map of logical names to globally unique S3 bucket names."
   type        = map(string)
 }
+
+variable "ec2_instances" {
+  description = "Map of EC2 instance definitions keyed by logical name."
+  type = map(object({
+    ami_id                = string
+    instance_type         = string
+    vpc_id                = string
+    subnet_id             = string
+    associate_public_ip   = bool
+    allowed_ingress_cidrs = list(string)
+    ssh_port              = optional(number, 22)
+    key_name              = optional(string)
+  }))
+  default = {}
+}
